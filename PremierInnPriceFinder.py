5f32f58a43b4b5685e6856989daf5192887cda37
DCutting23 <daniel.cutting@live.co.uk>
2023-09-30 22:30:27 +0100

    message

--- PremierInnPriceFinder.py
+++ PremierInnPriceFinder.py
@@ -1,6 +1,6 @@
 
 def URL_Constuctor(date): #date format dd/mm/yyyy
-    #URL = "https://www.premierinn.com/gb/en/search.html?searchModel.searchTerm=North%20Shields,%20UK&PLACEID=ChIJ4VnkDRFufkgRAkgbSvII-k4&ARRdd=30&ARRmm=9&ARRyyyy=2023&NIGHTS=1&ROOMS=1&ADULT1=1&CHILD1=0&COT1=0&INTTYP1=DB&BOOKINGCHANNEL=WEB&SORT=1&VIEW=2"
+    #URL example = "https://www.premierinn.com/gb/en/search.html?searchModel.searchTerm=North%20Shields,%20UK&PLACEID=ChIJ4VnkDRFufkgRAkgbSvII-k4&ARRdd=30&ARRmm=9&ARRyyyy=2023&NIGHTS=1&ROOMS=1&ADULT1=1&CHILD1=0&COT1=0&INTTYP1=DB&BOOKINGCHANNEL=WEB&SORT=1&VIEW=2"
     date_day = date[0:2]
     date_month = date[3:5]
     date_year = date[6:10]
